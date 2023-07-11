a469cfad33c2c564d92cffa5b8b65817121b0b1f
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2023-07-11 20:37:21 +0300

    Added Choice 2

--- Assignment 2/assignment_02_Mohammad_Abdelkhalek.py
+++ Assignment 2/assignment_02_Mohammad_Abdelkhalek.py
@@ -2,17 +2,15 @@
 # Mohammad Abdelkhalek Assignment 02 #
 ######################################
 
-"""def displayMenu():
+def displayMenu():
     print("1. Count Digits \n" +
           "2. Find Max \n" +
           "3. Count Tags \n" +
           "4. Exit")
     print("- " * 15, end="\n")
-    print("Enter a choice: ")"""
-    
-#displayMenu()
+    print("Enter a choice: ")
 
-######################################
+##################################################################################
 
 #Choice 1
 
@@ -22,6 +20,19 @@ def countDigits(integer):
         return 1
     else:
         count = 1 + countDigits(integer // 10)
-    return count
-       
-print(countDigits(252))
+    return count      
+
+##################################################################################
+
+#Choice 2
+
+def findMax(list1):    
+    if len(list1) == 1:
+        return list1[0]    
+    else:
+        max = findMax(list1[1:])
+        if list1[0] > max:  
+            return list1[0]
+        else:
+            return max
+
