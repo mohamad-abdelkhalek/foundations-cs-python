5ba30d3d56a1e6eb54c092ec3d0429207147c241
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2023-07-11 18:37:28 +0300

    Added Choice 1

--- Assignment 2/assignment_02_Mohammad_Abdelkhalek.py
+++ Assignment 2/assignment_02_Mohammad_Abdelkhalek.py
@@ -2,12 +2,26 @@
 # Mohammad Abdelkhalek Assignment 02 #
 ######################################
 
-def displayMenu():
+"""def displayMenu():
     print("1. Count Digits \n" +
           "2. Find Max \n" +
           "3. Count Tags \n" +
           "4. Exit")
     print("- " * 15, end="\n")
-    print("Enter a choice: ")
+    print("Enter a choice: ")"""
     
-displayMenu()
+#displayMenu()
+
+######################################
+
+#Choice 1
+
+def countDigits(integer):
+    count = 0
+    if integer < 10:
+        return 1
+    else:
+        count = 1 + countDigits(integer // 10)
+    return count
+       
+print(countDigits(252))
