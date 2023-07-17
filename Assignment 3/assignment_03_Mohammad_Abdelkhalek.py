c2da99a286986dc03b16d63ef2b639f359627c43
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2023-07-17 20:42:02 +0300

    Added Choice 1

--- Assignment 3/assignment_03_Mohammad_Abdelkhalek.py
+++ Assignment 3/assignment_03_Mohammad_Abdelkhalek.py
@@ -7,4 +7,14 @@
 def displayMenu():
     print("1. Sum Tuples\n2. Export JSON\n3. Import JSON\n4. Exit")
     print("- " * 15)
-    
+
+##################################################################
+
+#Choice 1
+
+def sumTuples(tuple1, tuple2):
+    lst = []
+    for i in range(len(tuple1)):
+        sum = tuple1[i] + tuple2[i]
+        lst.append(sum)
+    return tuple(lst)
