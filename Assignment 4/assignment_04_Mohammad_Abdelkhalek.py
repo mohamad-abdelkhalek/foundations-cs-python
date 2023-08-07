7fc8df0628d41808039f5b69ff90699df0b3b0ec
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2023-08-07 20:51:54 +0300

    Menu Added

---
+++ Assignment 4/assignment_04_Mohammad_Abdelkhalek.py
@@ -0,0 +1,14 @@
+######################################
+# Mohammad Abdelkhalek Assignment 04 #
+######################################
+
+
+def displayMenu():
+    print("1. Add a user to the platform\n"
+          "2. Remove a user from the platform\n"
+          "3. Send a friend request to another user\n"
+          "4. Remove a friend from your list\n"
+          "5. View your list of friends\n"
+          "6. View the list of users on the platform\n"
+          "7. Exit")
+    
