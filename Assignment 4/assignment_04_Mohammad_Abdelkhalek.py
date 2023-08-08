e99aa19fd8ae1fa81f3078a272b667e03dbd900a
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2023-08-08 18:51:49 +0300

    Choice 1 added

--- Assignment 4/assignment_04_Mohammad_Abdelkhalek.py
+++ Assignment 4/assignment_04_Mohammad_Abdelkhalek.py
@@ -11,4 +11,41 @@ def displayMenu():
           "5. View your list of friends\n"
           "6. View the list of users on the platform\n"
           "7. Exit")
+
+
+class Graph:
+
+    def __init__(self, vertices):
+        self.adj_list = {}
+        self.vertices = vertices
+
+        for vertex in range(vertices):
+            self.adj_list[vertex] = []
+
+
+    def addUser(self, username):
+        if username in self.adj_list:
+            print(f"Username '{username}' is already taken. Please choose another username.")
+        else:
+            self.adj_list[username] = []
+            print(f"User '{username}' has been added to the platform.")
     
+    
+    def DFS(self, v):
+        visited = [False] * self.vertices
+        stack = [v]
+
+        while False in visited:
+            if not stack:
+                for i in range(self.vertices):
+                    if not visited[i]:
+                        stack.append(i)
+
+            else:
+                v = stack.pop()
+                if not visited[v]:
+                    visited[v] = True
+                    print(v, end=' ')
+                    for w in self.adj_list[v]:
+                        if not visited[w]:
+                            stack.append(w)
